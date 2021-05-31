f52621a63ade995ea8eec37195603a0282867bc2
aviadtzemah <aviad896@gmail.com>
2021-05-31 15:43:41 +0300

    fixed the functionality of the cart

--- Client/src/Pages/Cart.js
+++ Client/src/Pages/Cart.js
@@ -8,26 +8,26 @@ import {Button, Card, CardGroup, Image, ListGroup, Spinner} from "react-bootstra
 import empty_cart from '../Images/harold_cart_empty.png'
 
 const products = [
-    {
-        name: "brioche",
-        productID: 1,
-        storeID: 1,
-        price: 50.5,
-        seller: "ma'afia",
-        categories: ["pastry", "tasty"],
-        rating: 5,
-        numReview: 200,
-    },
-    {
-        name: "eclair",
-        productID: 2,
-        storeID: 2,
-        price: 50.5,
-        seller: "ma'afia2",
-        categories: ["pastry", "tasty"],
-        rating: 4.5,
-        numReview: 300,
-    }
+        {
+            name: "brioche",
+            productID: 1,
+            storeID: 1,
+            price: 50.5,
+            seller: "ma'afia",
+            categories: ["pastry", "tasty"],
+            rating: 5,
+            numReview: 200,
+        },
+        {
+            name: "eclair",
+            productID: 2,
+            storeID: 2,
+            price: 50.5,
+            seller: "ma'afia2",
+            categories: ["pastry", "tasty"],
+            rating: 4.5,
+            numReview: 300,
+        }
     ]
 
 class Cart extends React.Component { //TODO check if the cart is empty and show an appropriate image
@@ -39,7 +39,8 @@ class Cart extends React.Component { //TODO check if the cart is empty and show 
             loaded: false,
 
         }
-        this.totalAmount =  0
+
+        this.totalAmount =  0;
         this.handleRemoveFromCart = this.handleRemoveFromCart.bind(this);
         this.handleQuantityChange = this.handleQuantityChange.bind(this);
         this.handleGetCartDetailsResponse = this.handleGetCartDetailsResponse.bind(this);
@@ -55,6 +56,7 @@ class Cart extends React.Component { //TODO check if the cart is empty and show 
     }
 
     componentDidMount() {
+        this.totalAmount = 0;
         Connection.sendGetCartDetails().then(this.handleGetCartDetailsResponse, Connection.handleReject);
     }
 
@@ -76,11 +78,18 @@ class Cart extends React.Component { //TODO check if the cart is empty and show 
         window.location.reload();
     }
 
-    addToTotal(price, amount){
-        let price_value = parseInt(price);
-        let amount_value = parseInt(amount);
-        this.totalAmount = this.totalAmount + price_value * amount_value;
-        // this.setState({totalAmount: this.state.totalAmount + price_value * amount_value})
+    addToTotal(price, amount, toZero){
+
+        if(!toZero){
+            let price_value = parseInt(price);
+            let amount_value = parseInt(amount);
+
+            this.totalAmount = this.totalAmount + price_value * amount_value;
+        }
+        else{
+            this.totalAmount = 0;
+        }
+
         return true;
     }
 
@@ -98,9 +107,9 @@ class Cart extends React.Component { //TODO check if the cart is empty and show 
                 {this.state.loaded && (this.state.cart.length !== 0) && <div id="card_page_content">
                     <div id="cart_items">
                         <ListGroup>
-                        {this.state.cart.map(({storeID, storeName, productsDTO, amounts}) => (
+                        {this.addToTotal(0, 0, true) && this.state.cart.map(({storeID, storeName, productsDTO, amounts}) => (
                             <div>
-                                {zip(productsDTO, amounts).map( entry => (this.addToTotal(entry[0].price, entry[1]) &&
+                                {zip(productsDTO, amounts).map( entry => (this.addToTotal(entry[0].price, entry[1], false) &&
                                     <ListGroup.Item><ProductEntryCart
                                         name = {entry[0].name}
                                         price = {entry[0].price}
@@ -108,7 +117,7 @@ class Cart extends React.Component { //TODO check if the cart is empty and show 
                                         amount = {entry[1]}
                                         seller = {storeName}
                                         productID = {entry[0].productID}
-                                        handler = {() => this.handleQuantityChange()}
+                                        handlerUpdate = {() => this.handleQuantityChange()}
                                         handlerRemove = {() => this.handleRemoveFromCart(storeID, entry[0].productID)}
                                     />
                                     </ListGroup.Item>
