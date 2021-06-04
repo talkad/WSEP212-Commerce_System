8c83196609bb608143ebb6a48f2074081fce3667
yaakovsh <48647853+yaakovsh@users.noreply.github.com>
2021-06-04 15:57:07 +0300

    implemented local db option, implemented caching, working on DAL thread for db access

---
+++ CommerceSystem/src/main/java/Server/DAL/DeleteProductDTO.java
@@ -0,0 +1,20 @@
+package Server.DAL;
+
+public class DeleteProductDTO extends ProductDTO{
+    private ProductDTO dto;
+
+    public DeleteProductDTO(ProductDTO dto){
+        this.dto = dto;
+    }
+
+    @Override
+    public boolean toDelete(){
+        return true;
+    }
+
+    @Override
+    public ProductDTO getDTO(){
+        return this.dto;
+    }
+
+}
